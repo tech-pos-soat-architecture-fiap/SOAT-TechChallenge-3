765e2c1d85f2cd82c0adcf62423d93c4abb48d3a
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2025-05-16 12:01:17 -0300

    Start Spring

---
+++ src/main/java/br/com/fiap/TechFood/TechFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.TechFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/TechFood/TechFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.TechFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
