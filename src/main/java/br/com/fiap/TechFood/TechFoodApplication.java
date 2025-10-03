1fd45ae744bc4eee6d8d8c3340c5ef300fb9c0c4
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2025-10-03 11:03:29 -0300

    chore: add a blank line for improved code readability

--- src/main/java/br/com/fiap/TechFood/TechFoodApplication.java
+++ src/main/java/br/com/fiap/TechFood/TechFoodApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class TechFoodApplication {
 
 	public static void main(String[] args) {
